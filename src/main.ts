914993659d257991c02a6afd83511ac2d8d38718
Roshu-3158 <roshandnikam3158@gmail.com>
2023-05-07 20:25:23 +0530

    resume added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,34 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// --------------------------scroll section active link-------------------------- 
 
-import { AppModule } from './app/app.module';
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+        navlinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*=' +id + ']')?.classList.add('active');
+
+        });
+    };
+  });
+  
+  
+  
+  // --------------------------sticky navbar--------------------------
+  
+  let header = document.querySelector('header');
+  header?.classList.toggle('sticky',window.scrollY > 100);
+  
+  
+};
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+
